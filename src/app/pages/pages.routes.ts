fe1812e6cbf4abff3ecc9c826b60784e1e5e1656
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-05-01 18:46:56 -0400

    feat: add RolesComponent with basic structure and tests
    - Created roles.component.ts for the Roles feature.
    - Added roles.component.html with a placeholder message.
    - Included roles.component.scss for styling (currently empty).
    - Implemented roles.component.spec.ts for unit testing the RolesComponent.

--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Documentation } from './documentation/documentation';
 import { Crud } from './crud/crud';
 import { Empty } from './empty/empty';
+import { RolesComponent } from './roles/roles.component';
 
 export default [
     { path: 'documentation', component: Documentation },
+    { path: 'roles', component: RolesComponent },
     { path: 'crud', component: Crud },
     { path: 'empty', component: Empty },
     { path: '**', redirectTo: '/notfound' }
---
+++ src/app/pages/roles/roles.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ProductService } from '../service/product.service';
+
+@Component({
+    selector: 'app-roles',
+    imports: [],
+    templateUrl: './roles.component.html',
+    styleUrl: './roles.component.scss',
+    providers: []
+})
+export class RolesComponent {}
