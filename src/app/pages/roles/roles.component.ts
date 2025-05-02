3d2a30447e9516e16c2fcea14c23a329b6d382f6
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-05-02 12:14:36 -0400

    feat: implement RolesComponent with toolbar and product management table

--- src/app/pages/roles/roles.component.ts
+++ src/app/pages/roles/roles.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { Product, ProductService } from '../service/product.service';
 import { ConfirmationService, MessageService } from 'primeng/api';
-import { ProductService } from '../service/product.service';
+import { Table, TableModule } from 'primeng/table';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { CurrencyPipe } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
 
+interface Column {
+    field: string;
+    header: string;
+    customExportHeader?: string;
+}
 @Component({
     selector: 'app-roles',
-    imports: [],
+    imports: [ToolbarModule, ButtonModule, TableModule, IconFieldModule, InputIconModule, InputTextModule],
     templateUrl: './roles.component.html',
     styleUrl: './roles.component.scss',
-    providers: []
+    providers: [MessageService, ConfirmationService]
 })
-export class RolesComponent {}
+export class RolesComponent {
+    constructor() {}
+
+    product!: Product;
+    products = signal<Product[]>([]);
+    selectedProducts!: Product[] | null;
+    cols!: Column[];
+
+    openNew() {
+        console.log('openNew button clicked');
+    }
+
+    onGlobalFilter(table: Table, event: Event) {
+        table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+    }
+
+    editProduct(product: Product) {
+        console.log('editProduct clicked', product);
+    }
+
+    deleteProduct(product: Product) {
+        console.log('deleteProduct clicked', product);
+    }
+}
