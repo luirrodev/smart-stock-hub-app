85b56dda75418739cacffc1a63656844c2e33160
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-05-05 22:40:52 -0400

    Refactor authentication module: Moved access, login, and error components to a new module structure; updated routing configuration accordingly.

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { Access } from './access';
-import { Login } from './login';
-import { Error } from './error';
+import { Access } from './pages/access';
+import { Login } from './pages/login';
+import { Error } from './pages/error';
 
 export default [
     { path: 'access', component: Access },
