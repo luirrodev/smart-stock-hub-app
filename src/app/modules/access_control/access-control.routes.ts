030163d176a6574c06e740fa96f971b9c03f225e
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-05-06 13:30:22 -0400

    feat: add users route to access control

--- src/app/modules/access_control/access-control.routes.ts
+++ src/app/modules/access_control/access-control.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { RolesComponent } from './pages/roles/roles.component';
+import { UsersComponent } from './pages/users/users.component';
 
-export default [{ path: 'roles', component: RolesComponent }] as Routes;
+export default [
+    { path: 'roles', component: RolesComponent },
+    { path: 'users', component: UsersComponent }
+] as Routes;
