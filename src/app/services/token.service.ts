886cfa39697a8092d24046ad4223bcdd3103c4b3
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2025-05-06 01:14:24 -0400

    Agregando cookies

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { getCookie, setCookie, removeCookie } from 'typescript-cookie';
 
 @Injectable({
     providedIn: 'root'
@@ -7,14 +8,17 @@ export class TokenService {
     constructor() {}
 
     saveToken(token: string) {
-        localStorage.setItem('token', token);
+        setCookie('stock-access-token', token, {
+            expires: 1,
+            path: '/'
+        });
     }
 
     getToken() {
-        return localStorage.getItem('token');
+        return getCookie('stock-access-token');
     }
 
     removeToken() {
-        localStorage.removeItem('token');
+        removeCookie('stock-access-token');
     }
 }
